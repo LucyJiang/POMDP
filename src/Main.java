c82d4eaa783e1a291460f9c5dc7aff538fa053b1
LucyJiang <Lucy.Jiang.Lulu@Gmail.com>
2015-03-06 23:36:57 +0000

    Init

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by LeoDong on 06/03/2015.
- */
-public class Main {
-    public static void main(String[] args){
-        System.out.print("DTW");
-    }
-}
--- src/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by LeoDong on 06/03/2015.
- */
-public class Test {
-}
